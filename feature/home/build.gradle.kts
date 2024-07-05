2cc32e6c3f327ca6fed1efa8995b2353fe4306b3
kursatkumsuz <kursatkumsuz@gmail.com>
2024-07-05 15:08:26 +0300

    Update gradle

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -48,4 +48,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Navigation
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(project(":navigation"))
 }
