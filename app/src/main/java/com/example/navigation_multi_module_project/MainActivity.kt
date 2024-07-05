5c4fc864ff5fc65695878aa112f08460f4666b15
kursatkumsuz <kursatkumsuz@gmail.com>
2024-07-05 15:55:44 +0300

    Define NavGraph & Inject navProvider

--- app/src/main/java/com/example/navigation_multi_module_project/MainActivity.kt
+++ app/src/main/java/com/example/navigation_multi_module_project/MainActivity.kt
@@ -11,15 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.NavProvider
+import com.example.navigation_multi_module_project.navigation.NavGraph
 import com.example.navigation_multi_module_project.ui.theme.NavigationmultimoduleprojectTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navProvider: NavProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NavigationmultimoduleprojectTheme {
-
+                val navController = rememberNavController()
+                NavGraph(navController = navController, navProvider = navProvider)
             }
         }
     }
