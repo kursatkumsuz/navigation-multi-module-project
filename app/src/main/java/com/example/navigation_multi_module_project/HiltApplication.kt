82f368e78cbe318348d9c350703275c9cce1cf0b
kursatkumsuz <kursatkumsuz@gmail.com>
2024-07-05 15:18:50 +0300

    Set up hilt

---
+++ app/src/main/java/com/example/navigation_multi_module_project/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.example.navigation_multi_module_project
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
